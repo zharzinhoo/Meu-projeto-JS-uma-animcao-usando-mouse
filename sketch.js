cdc3a38fa102accbfc2c790c840ad5db6e9b507e
zharzinhoo <134728845+zharzinhoo@users.noreply.github.com>
2023-05-26 15:32:41 -0300

    js

--- sketch.js
+++ sketch.js
@@ -1,42 +1,41 @@
-
     //by: zhar
 
-    let cor; 
-    let posiCaoHorizontal; // x
-    let posicaoVertical; // y
-    
-    
-    function setup() {
-      createCanvas(1920, 1080);
-      background("white");
-      cor = color(random(0, 255),random(0, 255), random(0, 255));
-      posiCaoHorizontal = 200;
-      posicaoVertical = 200;
+let cor; 
+let posiCaoHorizontal; // x
+let posicaoVertical; // y
+
+
+function setup() {
+  createCanvas(1920, 1080);
+  background("white");
+  cor = color(random(0, 255),random(0, 255), random(0, 255));
+  posiCaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw() {
+  
+  fill(cor);
+  circle(posiCaoHorizontal, posicaoVertical, 40)
+ 
+  if (mouseX < posiCaoHorizontal) {
+    posiCaoHorizontal = posiCaoHorizontal - 1;
     }
     
-    function draw() {
-      
-      fill(cor);
-      circle(posiCaoHorizontal, posicaoVertical, 40)
-     
-      if (mouseX < posiCaoHorizontal) {
-        posiCaoHorizontal = posiCaoHorizontal - 1;
-        }
-        
-      
-      if (mouseX > posiCaoHorizontal) {
-        posiCaoHorizontal = posiCaoHorizontal + 1;
-        }
-      
-        if (mouseY < posicaoVertical ) {
-        posicaoVertical--;
-      }
-      
-     if (mouseY > posicaoVertical ) {
-        posicaoVertical++;
-      }
-      
-     if (mouseIsPressed) {
-      cor = color(random(0, 255),random(0, 255), random(0, 255), random(0, 100));
-       }
+  
+  if (mouseX > posiCaoHorizontal) {
+    posiCaoHorizontal = posiCaoHorizontal + 1;
     }
+  
+    if (mouseY < posicaoVertical ) {
+    posicaoVertical--;
+  }
+  
+ if (mouseY > posicaoVertical ) {
+    posicaoVertical++;
+  }
+  
+ if (mouseIsPressed) {
+  cor = color(random(0, 255),random(0, 255), random(0, 255), random(0, 100));
+   }
+}
