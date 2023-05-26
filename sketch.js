399ad1c189d520eb953c84d54113ee904a728bc0
zharzinhoo <134728845+zharzinhoo@users.noreply.github.com>
2023-05-26 15:23:31 -0300

    JS

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
     
     
     function setup() {
-      createCanvas(400, 400);
+      createCanvas(1920, 1080);
       background("white");
       cor = color(random(0, 255),random(0, 255), random(0, 255));
       posiCaoHorizontal = 200;
